7ded2bf27d68d49f4d63ae84fec369f44f6b2ee3
cshekar2k10 <cshekar2k10@gmail.com>
2023-04-03 00:25:53 +0530

    Updating learn-terraform

--- 03-vars/vars.tf
+++ 03-vars/vars.tf
@@ -5,3 +5,18 @@ variable "sample" {
 output "sample" {
   value = var.sample
 }
+
+# String Data Type
+variable "sample1" {
+  default = "Hello World"
+}
+
+# Number Data Type
+variable "sample2" {
+  default = 100
+}
+
+# Boolean Data Type
+variable "sample3" {
+  default = true
+}
