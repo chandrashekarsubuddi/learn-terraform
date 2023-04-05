688ed5e7961ba9ceeec6e25dec094cd37baecd1f
cshekar2k10 <cshekar2k10@gmail.com>
2023-04-05 16:29:39 +0530

    Updating learn-terraform

--- 05-remote-state/sample.tf
+++ 05-remote-state/sample.tf
@@ -14,8 +14,8 @@ resource "aws_instance" "web" {
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_tls.id]
 
-  tags = {
-    name = "test-centos8"
+  tag = {
+    Name = "test-centos8"
   }
 }
 
