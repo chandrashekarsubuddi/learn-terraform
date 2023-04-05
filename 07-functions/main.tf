8f9cd2b049af492302f67d958cd265be780a2bf5
cshekar2k10 <cshekar2k10@gmail.com>
2023-04-05 18:27:48 +0530

    Updating learn-terraform

--- 07-functions/main.tf
+++ 07-functions/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami = data.aws_ami.centos8.id
   instance_type = "t3.micro"
-  
+
   tags = {
     Name = "test-centos8"
   }
@@ -13,4 +13,8 @@ data "aws_ami" "centos8" {
   name_regex       = "Centos-8-DevOps-Practice"
   owners           = ["973714476881"]
 
+}
+
+output "instance_profile" {
+  value = aws_instance.web.outpost_arn
 }
