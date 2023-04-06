1faae78f02b728174b8314883da335cb193ed846
cshekar2k10 <cshekar2k10@gmail.com>
2023-04-06 15:13:32 +0530

    Updating learn-terraform

--- 08-loops/for-each/main.tf
+++ 08-loops/for-each/main.tf
@@ -20,7 +20,9 @@ output "publicip" {
   #value = aws_instance.web.public_ip
 
   #with count
-  value = aws_instance.web
+  value = {
+    for k,v in aws_instance.web : k => v.public_ip
+  }
 }
 
 variable "components" {
